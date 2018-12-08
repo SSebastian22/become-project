b82489947f18ed91ea0008aee80eb1704d7fc472
Sebastian <stoleru_sebastian@yahoo.com>
2018-12-08 13:40:41 +0200

    123

---
+++ app.js
@@ -0,0 +1,16 @@
+document.body.onkeyup = function(e){
+    if(e.keyCode===13) item();
+}
+
+function item() {
+    const item = document.getElementById("input").value;
+    const ul = document.getElementById("list");
+    const li = document.createElement("li");
+    li.appendChild(document.createTextNode("-"+item));
+    ul.appendChild(li);
+    document.getElementById("input").value="";
+    li.onclick=removeItem;
+}
+function removeItem(e) {
+    e.target.parentElement.removeChild(e.target);
+}
